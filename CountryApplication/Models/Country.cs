a0d6fd7a398226fdde15a34e4cfb7c2d163180ca
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-04 18:21:03 +0200

    [Add] Application models

---
+++ CountryApplication/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BuildingBlock.DataAccess;
+using BuildingBlock.DataAccess.Abstractions;
+
+namespace CountryApplication.Models
+{
+    public class Country : Entity, IAggregateRoot
+    {
+        public Country(string name,
+            string code)
+        {
+            Uuid = Guid.NewGuid();
+
+            Name = name;
+
+            Code = code;
+        }
+
+        public Guid Uuid { get; }
+
+        public string Name { get; }
+
+        public string Code { get; }
+    }
+}
