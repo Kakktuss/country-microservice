425b5902c09f6c01089c6cbf24fd7640cdba70a1
Kaktus <benjaminmanderveldepro@gmail.com>
2020-10-04 18:23:11 +0200

    [Add] Application repositories DI register module

---
+++ CountryApplication/RepositoryAutofacModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using CountryApplication.EntityFrameworkDataAccess.Repositories;
+
+namespace CountryApplication
+{
+    public class RepositoryAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // builder.RegisterType<CountryRe>()
+
+            builder.RegisterType<CountryRepository>()
+                .As<ICountryRepository>()
+                .AsSelf();
+        }
+    }
+}
